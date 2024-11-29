25c843f3976ea365af6ebd1db60c9fdf3604ad46
Marina Pollak <pollak@colum.edu>
2024-11-29 16:07:54 -0600

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SearchingMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
